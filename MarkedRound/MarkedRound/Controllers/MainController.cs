9ac86fb5f8a24ae44c7c84d2bcdc30dad6ab0d85
HakanSahin11 <hakan.sahin994@gmail.com>
2020-08-05 10:56:10 +0200

    Added change user method section

--- MarkedRound/MarkedRound/Controllers/MainController.cs
+++ MarkedRound/MarkedRound/Controllers/MainController.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Authentication;
 using System.Threading.Tasks;
 using MarkedRound.Model;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using MongoDB.Driver.Linq;
+using static MarkedRound.HelpClasses.UpdateUser;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+/*Notes:
+ * Update Method:  ChangeUserInput(id, Document, StrInput, IntInput, collection)
+ * Exempel: ChangeUserInput(1, "PhoneNumber", null, 6666, "Users");
+ * 
+ */
+
+
+
 
 namespace MarkedRound.Controllers
 {
@@ -18,8 +29,12 @@ namespace MarkedRound.Controllers
     {
         public static List<UserModel> GetAllUsers(int? id, string section)
         {
+            /* Azure connectionstring
+             @"mongodb://markedround:ssJnR833qFMonYSH6h3iYXwiCGGQ06SgvbPW72LKstejR1lGUWtCy5eZG7qzNPO00xVKhiC5jNVUo8oUge5p6Q==@markedround.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@markedround@?";
+            */
+                       
             //Database Connection
-            IMongoDatabase client = new MongoClient($"mongodb://{"adminUser"}:{"silvereye"}@localhost:27017").GetDatabase("silkevejen");
+               IMongoDatabase client = new MongoClient($"mongodb://{"adminUser"}:{"silvereye"}@localhost:27017").GetDatabase("silkevejen");
 
             //Collection Query
             IMongoQueryable<UserModel> usageQuery;
@@ -36,12 +51,9 @@ namespace MarkedRound.Controllers
                              where c._id == id
                             select c;
             }
-
             var ListOfUsers = new List<UserModel>();
-           
-
             ListOfUsers.AddRange(usageQuery);
-                return ListOfUsers;
+            return ListOfUsers;
         }
 
         // GET: api/<MainController>
---
+++ MarkedRound/MarkedRound/HelpClasses/UpdateUser.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarkedRound.HelpClasses
+{
+    public class UpdateUser
+    {
+        public static bool ChangeUserInput(int id, string Document, string StrInput, int? IntInput, string collection)
+        {
+            try
+            {
+                //DB
+                IMongoDatabase client = new MongoClient($"mongodb://{"adminUser"}:{"silvereye"}@localhost:27017").GetDatabase("silkevejen");
+                var BsonColl = client.GetCollection<BsonDocument>(collection);
+
+                //Section for changing users input on DB
+                FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq("_id", id);
+                UpdateDefinition<BsonDocument> update;
+                if (StrInput != null)
+                {
+                    // Used for when a string value is given
+                    update = Builders<BsonDocument>.Update.Set(Document, StrInput);
+                }
+                else
+                {
+                    // Used for when an int value is given
+                    update = Builders<BsonDocument>.Update.Set(Document, IntInput);
+                }
+                BsonColl.UpdateOne(filter, update);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
